0755907478aca523f37c6d934492aecae5c04fe3
blay09 <blay09@hotmail.de>
2019-02-21 10:17:29 +0100

    Use INPUT_INVALID constant

--- src/main/java/net/blay09/mods/gravelminer/client/GravelMinerKeyBindings.java
+++ src/main/java/net/blay09/mods/gravelminer/client/GravelMinerKeyBindings.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.client.registry.ClientRegistry;
 
 public class GravelMinerKeyBindings {
 
-    public static final KeyBinding keyToggle = new KeyBinding("key.gravelminer.toggle", KeyConflictContext.IN_GAME, KeyModifier.NONE, InputMappings.Type.KEYSYM.getOrMakeInput(0), "key.categories.gravelminer");
+    public static final KeyBinding keyToggle = new KeyBinding("key.gravelminer.toggle", KeyConflictContext.IN_GAME, KeyModifier.NONE, InputMappings.INPUT_INVALID, "key.categories.gravelminer");
 
     public static void register() {
         ClientRegistry.registerKeyBinding(keyToggle);
