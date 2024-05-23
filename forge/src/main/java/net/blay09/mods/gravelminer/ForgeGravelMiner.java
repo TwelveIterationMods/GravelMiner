118941075ff98319922d6ab81ca54781337924a5
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-05-23 15:06:08 +0200

    fix(1.20.6): Fix Forge builds

--- forge/src/main/java/net/blay09/mods/gravelminer/ForgeGravelMiner.java
+++ forge/src/main/java/net/blay09/mods/gravelminer/ForgeGravelMiner.java
@@ -1,6 +1,7 @@
 package net.blay09.mods.gravelminer;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.client.BalmClient;
 import net.blay09.mods.gravelminer.client.GravelMinerClient;
 import net.minecraftforge.api.distmarker.Dist;
@@ -10,8 +11,8 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(GravelMiner.MOD_ID)
 public class ForgeGravelMiner {
     public ForgeGravelMiner() {
-        Balm.initialize(GravelMiner.MOD_ID, GravelMiner::initialize);
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(GravelMiner.MOD_ID, GravelMinerClient::initialize));
+        Balm.initialize(GravelMiner.MOD_ID, EmptyLoadContext.INSTANCE, GravelMiner::initialize);
+        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(GravelMiner.MOD_ID, EmptyLoadContext.INSTANCE, GravelMinerClient::initialize));
     }
 
 }
