2c8f93849a604f4dd92f75c38e82e500fe1d29f6
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-17 20:42:19 +0100

    feat: Update to Minecraft 1.21.5

--- common/src/main/java/net/blay09/mods/gravelminer/GravelMinerConfig.java
+++ common/src/main/java/net/blay09/mods/gravelminer/GravelMinerConfig.java
@@ -1,24 +1,59 @@
 package net.blay09.mods.gravelminer;
 
+import com.google.common.collect.Lists;
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.config.reflection.Comment;
+import net.blay09.mods.balm.api.config.reflection.Config;
+import net.blay09.mods.balm.api.config.reflection.NestedType;
 
+import java.util.List;
+
+@Config(GravelMiner.MOD_ID)
 public class GravelMinerConfig {
-    public static GravelMinerConfigData getActive() {
-        return Balm.getConfig().getActive(GravelMinerConfigData.class);
+
+    public Common common = new Common();
+    public Client client = new Client();
+
+    public static class Common {
+        @NestedType(String.class)
+        @Comment("Blocks that will fall and break into items when hitting a non-solid block. Format: modid:name")
+        public List<String> gravelBlocks = Lists.newArrayList("minecraft:gravel");
+
+        @Comment("If set to true, the mod will trigger when mining gravel as well, instead of only when mining a non-gravel block below gravel.")
+        public boolean triggerOnGravel = true;
+
+        @Comment("If set to true, GravelMiner will only be active for users who install the mod on their clients (for use in servers).")
+        public boolean isOptIn = true;
+
+        @Comment("If set to true, gravel mined via GravelMiner will have a chance of dropping as flint (server-side only).")
+        public boolean rollFlintChance = true;
+    }
+
+    public static class Client {
+        @Comment("Whether GravelMiner is currently enabled for this client (toggled via keybind)")
+        public boolean isEnabled = true;
+
+        @Comment("Defines when GravelMiner should activate. Either ALWAYS, WHEN_SNEAKING or WHEN_NOT_SNEAKING")
+        public GravelMinerActivation activation = GravelMinerActivation.ALWAYS;
+    }
+
+
+    public static GravelMinerConfig getActive() {
+        return Balm.getConfig().getActiveConfig(GravelMinerConfig.class);
     }
 
     public static void initialize() {
-        Balm.getConfig().registerConfig(GravelMinerConfigData.class, null);
+        Balm.getConfig().registerConfig(GravelMinerConfig.class);
     }
 
     public static void setEnabled(boolean enabled) {
-        Balm.getConfig().updateConfig(GravelMinerConfigData.class, config -> {
+        Balm.getConfig().updateLocalConfig(GravelMinerConfig.class, config -> {
             config.client.isEnabled = enabled;
         });
     }
 
     public static GravelMinerClientSetting getClientSetting() {
-        GravelMinerConfigData config = getActive();
+        GravelMinerConfig config = getActive();
         if (config.client.isEnabled && config.client.activation == GravelMinerActivation.ALWAYS) {
             return GravelMinerClientSetting.ENABLED;
         } else if (config.client.isEnabled && config.client.activation == GravelMinerActivation.WHEN_SNEAKING) {
