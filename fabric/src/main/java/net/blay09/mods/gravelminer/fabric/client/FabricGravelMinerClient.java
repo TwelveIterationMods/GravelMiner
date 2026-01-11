07b9a2d7d509b3b45ef6e22198ddd115ba4c4fec
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-01-11 12:54:42 +0100

    debug: Just a quick test to see if remapping works

--- fabric/src/main/java/net/blay09/mods/gravelminer/fabric/client/FabricGravelMinerClient.java
+++ fabric/src/main/java/net/blay09/mods/gravelminer/fabric/client/FabricGravelMinerClient.java
@@ -4,12 +4,17 @@ import net.blay09.mods.balm.client.BalmClient;
 import net.blay09.mods.balm.fabric.platform.runtime.FabricLoadContext;
 import net.blay09.mods.gravelminer.GravelMiner;
 import net.blay09.mods.gravelminer.client.GravelMinerClient;
+
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
 
 public class FabricGravelMinerClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
         BalmClient.initializeMod(GravelMiner.MOD_ID, FabricLoadContext.INSTANCE, GravelMinerClient::initialize);
+
+        KeyBindingHelper.registerKeyBinding(new KeyMapping("", 0, null));
     }
 }
