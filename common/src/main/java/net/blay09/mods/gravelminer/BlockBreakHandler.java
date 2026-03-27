c48650ca9e92920d87adece5540e8b6d48842419
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-27 11:26:52 +0100

    refactor: Remove jetbrains annotations

--- common/src/main/java/net/blay09/mods/gravelminer/BlockBreakHandler.java
+++ common/src/main/java/net/blay09/mods/gravelminer/BlockBreakHandler.java
@@ -10,7 +10,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 public class BlockBreakHandler {
 
